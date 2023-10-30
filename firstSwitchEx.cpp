d9eb581e2084941afb4ce077372ad28257fb6386
Moya Butts <133909021+mcbutts@users.noreply.github.com>
2023-10-30 15:37:16 -0400

    firstSwitchEx.cpp

---
+++ firstSwitchEx.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int num1;
+  cin >> num1;
+
+  switch (num1) {
+    case : 1 cout << "ONE" << endl;
+    break;
+    case : 2 cout << "TWO" << endl;
+    break;
+    case : 3 cout << "THREE" << endl;
+    break;
+    case : 4 cout << "FOUR" << endl;
+    break;
+    default : cout << "INVALID NUMBER" << endl;
+    break;
+  }
